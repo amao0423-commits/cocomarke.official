6bee0d180a4de9b8e5002dbf70b4a811001f9558
AndoAoi <andoaoi@AndoAoinoMac-mini.local>
2026-05-15 16:36:02 +0900

    fix: separate html templates from static assets to enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   publicDir: './old',
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: ['./templates/**/*'],
+  }),
 });

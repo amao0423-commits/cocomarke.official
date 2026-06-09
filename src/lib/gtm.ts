6384a67b54d79eb1603155d1501cc670dfb81dc8
AndoAoi <andoaoi@AndoAoinoMac-mini.local>
2026-06-09 10:31:09 +0900

    Install Google Tag Manager

---
+++ src/lib/gtm.ts
@@ -0,0 +1,24 @@
+const GTM_ID = 'GTM-TRWPWMJ4';
+
+const GTM_HEAD = `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');</script>
+<!-- End Google Tag Manager -->`;
+
+const GTM_BODY = `<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`;
+
+export function injectGtm(html: string) {
+  if (html.includes(GTM_ID)) return html;
+
+  const withHead = html.includes('</head>')
+    ? html.replace('</head>', `${GTM_HEAD}</head>`)
+    : html;
+
+  return withHead.replace(/<body([^>]*)>/i, `<body$1>${GTM_BODY}`);
+}

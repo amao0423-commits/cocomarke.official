981c357058e7be71a21af655bc141e851db6dcb2
AndoAoi <andoaoi@AndoAoinoMac-mini.local>
2026-06-09 11:01:36 +0900

    Install Meta Pixel

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -1,4 +1,5 @@
 const GTM_ID = 'GTM-TRWPWMJ4';
+const META_PIXEL_ID = '1232285451938211';
 
 const GTM_HEAD = `<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -13,12 +14,39 @@ const GTM_BODY = `<!-- Google Tag Manager (noscript) -->
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->`;
 
+const META_PIXEL_HEAD = `<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->`;
+
 export function injectGtm(html: string) {
-  if (html.includes(GTM_ID)) return html;
+  let result = html;
 
-  const withHead = html.includes('</head>')
-    ? html.replace('</head>', `${GTM_HEAD}</head>`)
-    : html;
+  if (!result.includes(GTM_ID)) {
+    result = result.includes('</head>')
+      ? result.replace('</head>', `${GTM_HEAD}</head>`)
+      : result;
+    result = result.replace(/<body([^>]*)>/i, `<body$1>${GTM_BODY}`);
+  }
 
-  return withHead.replace(/<body([^>]*)>/i, `<body$1>${GTM_BODY}`);
+  if (!result.includes(META_PIXEL_ID)) {
+    result = result.includes('</head>')
+      ? result.replace('</head>', `${META_PIXEL_HEAD}</head>`)
+      : result;
+  }
+
+  return result;
 }
